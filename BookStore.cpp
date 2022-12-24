19910499faedcd3a52e08ca9329b672697bca00e
Emad Albalas <ealbalas@csu.fullerton.edu>
2022-12-23 17:51:50 -0800

    created bookstore class with functions to find book, access and change balance, check stock, buy inventory, sell inventory

--- BookStore.cpp
+++ BookStore.cpp
@@ -1,1 +1,63 @@
+#include "BookStore.hpp"
 
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+namespace CPSC131::BookStore
+{
+    BookStore::BookStore() {}
+
+    void BookStore::adjustAccountBalance(int adjustment)
+    {
+        account_balance_ +=adjustment;
+    }
+
+    int BookStore::getAccountBalance()
+    {
+        return this->account_balance_;
+    }
+
+    DoublyLinkedList::DoublyLinkedList<Book>::Iterator BookStore::findBook(string isbn)
+    {}
+
+    bool BookStore::bookExists(string isbn)
+    {}
+
+    size_t BookStore::getBookStock(string isbn)
+    {}
+
+    Book& BookStore::getBook(string isbn)
+    {}
+
+    void BookStore::purchaseInventory(const Book& book)
+    {}
+
+    void BookStore::purchaseInventory(
+        string title, string author, string isbn,
+        size_t price_in_cents,
+        size_t unit_count
+    )
+    {}
+
+    void BookStore::sellToCustomer(Book& book, size_t price_in_cents, size_t quantity)
+    {}
+
+    void BookStore::sellToCustomer(string isbn, size_t price_in_cents, size_t quantity)
+    {}
+
+    void BookStore::printInventroy()
+    {
+        cout << "*** Book Store Inventory ***" << endl;
+        for (auto &books : bookList)
+        {
+            string title = books.getTitle();
+            cout << "\"" << books.getTitle() << "\""
+            << ", by " << books.getAuthor()
+            << " [" << books.getIsbn() << "] "
+            << "(" << books.getStock() << " in stock)" << endl;
+        }
+    }
+}
--- BookStore.hpp
+++ BookStore.hpp
@@ -1,1 +1,45 @@
+#ifndef BOOK_STORE_HPP
+#define BOOK_STORE_HPP
 
+#include "DoublyLinkedList.hpp"
+#include "Book.hpp"
+
+using namespace std;
+
+namespace CPSC131
+{
+    namespace BookStore
+    {
+        class BookStore
+        {
+            public:
+                BookStore();
+
+                void adjustAccountBalance(int adjustment);
+                int getAccountBalance();
+
+                Book& getBook(string isbn);
+                DoublyLinkedList::DoublyLinkedList<Book>::Iterator findBook(string isbn);
+                bool bookExists(string isbn);
+                size_t getBookStock(string isbn);
+
+                void purchaseInventory(const Book& book);
+                void purchaseInventory(
+                    string title, string author, string isbn,
+                    size_t price_in_cents,
+                    size_t unit_count
+                );
+
+                void sellToCustomer(Book& book, size_t price_in_cents, size_t quantity);
+                void sellToCustomer(string isbn, string author, size_t quantity);
+
+                void printInventroy();
+
+            private:
+                ::CPSC131::DoublyLinkedList:DoublyLinkedList<Book> bookList;
+
+                int account_balance_ = 0;
+        };
+    }
+}
+#endif
